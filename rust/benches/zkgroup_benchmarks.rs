70be1204d574b8d171ad77f8d1aefa9317a98718
Gerardo Di Giacomo <gdg@signal.org>
2020-04-28 12:21:42 -0700

    Update repositories

--- rust/benches/zkgroup_benchmarks.rs
+++ rust/benches/zkgroup_benchmarks.rs
@@ -8,6 +8,82 @@ use zkgroup::crypto;
 use curve25519_dalek::ristretto::RistrettoPoint;
 use sha2::Sha256;
 
+fn benchmark_integration_auth(c: &mut Criterion) {
+    let server_secret_params = zkgroup::ServerSecretParams::generate(zkgroup::TEST_ARRAY_32);
+    let server_public_params = server_secret_params.get_public_params();
+
+    let master_key = zkgroup::groups::GroupMasterKey::new(zkgroup::TEST_ARRAY_32_1);
+    let group_secret_params =
+        zkgroup::groups::GroupSecretParams::derive_from_master_key(master_key);
+    let group_public_params = group_secret_params.get_public_params();
+
+    // Random UID and issueTime
+    let uid = zkgroup::TEST_ARRAY_16;
+    let redemption_time = 123456u32;
+
+    // SERVER
+    // Issue credential
+    let randomness = zkgroup::TEST_ARRAY_32_2;
+    let auth_credential_response =
+        server_secret_params.issue_auth_credential(randomness, uid, redemption_time);
+
+    c.bench_function("issue_auth_credential", |b| {
+        b.iter(|| server_secret_params.issue_auth_credential(randomness, uid, redemption_time))
+    });
+
+    // CLIENT
+    let auth_credential = server_public_params
+        .receive_auth_credential(uid, redemption_time, &auth_credential_response)
+        .unwrap();
+
+    c.bench_function("receive_auth_credential", |b| {
+        b.iter(|| {
+            server_public_params
+                .receive_auth_credential(uid, redemption_time, &auth_credential_response)
+                .unwrap()
+        })
+    });
+
+    // Create and decrypt user entry
+    let uuid_ciphertext = group_secret_params.encrypt_uuid(uid);
+    let plaintext = group_secret_params.decrypt_uuid(uuid_ciphertext).unwrap();
+    assert!(plaintext == uid);
+
+    // Create and receive presentation
+    let randomness = zkgroup::TEST_ARRAY_32_5;
+
+    let presentation = server_public_params.create_auth_credential_presentation(
+        randomness,
+        group_secret_params,
+        auth_credential,
+    );
+
+    c.bench_function("create_auth_credential_presentation", |b| {
+        b.iter(|| {
+            server_public_params.create_auth_credential_presentation(
+                randomness,
+                group_secret_params,
+                auth_credential,
+            )
+        })
+    });
+
+    let presentation_bytes = &bincode::serialize(&presentation).unwrap();
+
+    //for b in presentation_bytes.iter() {
+    //    print!("0x{:02x}, ", b);
+    //}
+    //assert!(AUTH_CREDENTIAL_PRESENTATION_RESULT[..] == presentation_bytes[..]);
+
+    c.bench_function("verify_auth_credential_presentation", |b| {
+        b.iter(|| {
+            server_secret_params
+                .verify_auth_credential_presentation(group_public_params, &presentation)
+                .unwrap();
+        })
+    });
+}
+
 // Copied and modified from tests/integration_tests.rs
 pub fn benchmark_integration_profile(c: &mut Criterion) {
     // Random UID and issueTime
@@ -36,6 +112,17 @@ pub fn benchmark_integration_profile(c: &mut Criterion) {
         uid,
         profile_key,
     );
+
+    c.bench_function("create_profile_key_credential_request_context", |b| {
+        b.iter(|| {
+            server_public_params.create_profile_key_credential_request_context(
+                randomness,
+                uid,
+                profile_key,
+            )
+        })
+    });
+
     let request = context.get_request();
 
     // SERVER
@@ -130,5 +217,9 @@ pub fn benchmark_integration_profile(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_integration_profile);
+criterion_group!(
+    benches,
+    benchmark_integration_profile,
+    benchmark_integration_auth
+);
 criterion_main!(benches);
